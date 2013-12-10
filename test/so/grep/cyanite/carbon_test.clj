bfc4384d76f3f99d693239a791216be4dc786219
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-12-10 21:40:54 +0100

    test rollups in carbon formatter

--- test/so/grep/cyanite/carbon_test.clj
+++ test/so/grep/cyanite/carbon_test.clj
@@ -1,7 +1,37 @@
 (ns so.grep.cyanite.carbon-test
   (:require [so.grep.cyanite.carbon :refer :all]
+            [so.grep.cyanite.config :refer [assoc-rollup-to]]
             [clojure.test           :refer :all]))
 
 (deftest formatter-test
   (testing "nil-transform"
-    (is (= nil (formatter [] "foo nan 1234")))))
+    (is (= nil (formatter [] "foo nan 501"))))
+
+  (testing "rollup-transform"
+    (is (= (list {:path "foo.bar"
+                  :rollup 10
+                  :period 3600
+                  :ttl    36000
+                  :time   500
+                  :metric 2.0})
+           (formatter (assoc-rollup-to  [{:rollup 10 :period 3600}])
+                      "foo.bar 2.0 501")))
+
+    (is (= (list {:path "foo.bar"
+                  :rollup 10
+                  :period 3600
+                  :ttl    36000
+                  :time   600
+                  :metric 2.0}
+                 {:path "foo.bar"
+                  :rollup 60
+                  :period 7200
+                  :ttl    432000
+                  :time   600
+                  :metric 2.0})
+           (formatter (assoc-rollup-to  [{:rollup 10 :period 3600}
+                                         {:rollup 60 :period 7200}])
+                      "foo.bar 2.0 601")))
+
+
+    ))
