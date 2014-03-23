4ac650c90bdbde7ad058b6722015e1e21870922e
Neil Prosser <neil.prosser@gmail.com>
2014-03-23 20:53:01 +0000

    Add support for carbon's shorthand rollup period (issue #2)

--- src/org/spootnik/cyanite/config.clj
+++ src/org/spootnik/cyanite/config.clj
@@ -1,6 +1,7 @@
 (ns org.spootnik.cyanite.config
   "Yaml config parser, with a poor man's dependency injector"
   (:require [clj-yaml.core         :refer [parse-string]]
+            [clojure.string        :refer [split]]
             [clojure.tools.logging :refer [error info debug]]))
 
 (def
@@ -29,6 +30,37 @@
   {:enabled true
    :host    "127.0.0.1"
    :port    8080})
+
+(defn to-seconds
+  "Takes a string containing a duration like 13s, 4h etc. and
+   converts it to seconds"
+  [s]
+  (let [[_ value unit] (re-matches #"^([0-9]+)([a-z])$" s)
+        quantity (Integer/valueOf value)]
+    (case unit
+      "s" quantity
+      "m" (* 60 quantity)
+      "h" (* 60 60 quantity)
+      "d" (* 24 60 60 quantity)
+      "w" (* 7 24 60 60 quantity)
+      "y" (* 365 24 60 60 quantity)
+      (throw (ex-info (str "unknown rollup unit: " unit) {})))))
+
+(defn convert-shorthand-rollup
+  "Converts an individual rollup to a {:rollup :period} pair"
+  [rollup]
+  (if (string? rollup)
+    (let [[rollup-string retention-string] (split rollup #":" 2)
+          rollup-secs (to-seconds rollup-string)
+          retention-secs (to-seconds retention-string)]
+      {:rollup rollup-secs :period (/ retention-secs rollup-secs)})
+    rollup))
+
+(defn convert-shorthand-rollups
+  "Where a rollup has been given in Carbon's shorthand form
+   convert it to a {:rollup :period} pair"
+  [rollups]
+  (map convert-shorthand-rollup rollups))
 
 (defn assoc-rollup-to
   "Enhance a rollup definition with a function to compute
@@ -86,5 +118,6 @@
         (update-in [:store] (partial merge default-store))
         (update-in [:store] get-instance :store)
         (update-in [:carbon] (partial merge default-carbon))
+        (update-in [:carbon :rollups] convert-shorthand-rollups)
         (update-in [:carbon :rollups] assoc-rollup-to)
         (update-in [:http] (partial merge default-http)))))
