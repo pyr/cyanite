470ab3979a5de7c1e435d42be3b8378ccc9771bc
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-03-26 11:58:35 +0100

    bump alia & java-driver

--- project.clj
+++ project.clj
@@ -5,16 +5,17 @@
             :url "https://github.com/pyr/cyanite/tree/master/LICENSE"}
   :maintainer {:email "pyr@spootnik.org"}
   :main org.spootnik.cyanite
-  :dependencies [[org.clojure/clojure       "1.6.0"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/tools.cli     "0.2.4"]
-                 [ring/ring-codec           "1.0.0"]
-                 [aleph                     "0.3.0"]
-                 [clj-yaml                  "0.4.0"]
-                 [cc.qbits/alia             "1.8.2"]
-                 [org.slf4j/slf4j-log4j12   "1.6.4"]
-                 [log4j/apache-log4j-extras "1.0"]
-                 [log4j/log4j               "1.2.16"
+  :dependencies [[org.clojure/clojure             "1.6.0"]
+                 [org.clojure/tools.logging       "0.2.6"]
+                 [org.clojure/tools.cli           "0.2.4"]
+                 [commons-logging/commons-logging "1.1.3"]
+                 [ring/ring-codec                 "1.0.0"]
+                 [aleph                           "0.3.0"]
+                 [clj-yaml                        "0.4.0"]
+                 [cc.qbits/alia                   "2.0.0-beta10"]
+                 [org.slf4j/slf4j-log4j12         "1.6.4"]
+                 [log4j/apache-log4j-extras       "1.0"]
+                 [log4j/log4j                     "1.2.16"
                   :exclusions [javax.mail/mail
                                javax.jms/jms
                                com.sun.jdkmk/jmxtools
