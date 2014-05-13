ad222940aa124b133f8ddbca38155ab6b5248254
Addison Higham <addisonj@gmail.com>
2014-05-13 13:13:17 -0600

    handle bad numbers in carbon handler

--- src/org/spootnik/cyanite/carbon.clj
+++ src/org/spootnik/cyanite/carbon.clj
@@ -8,12 +8,23 @@
             [gloss.core                 :refer [string]]
             [lamina.core                :refer [receive-all map* siphon]]))
 
+(defn parse-num
+  "parse a number into the given value, return the
+  default value if it fails"
+  [parse default number]
+  (try (parse number)
+    (catch NumberFormatException e
+      (debug "got an invalid number" number (.getMessage e))
+      default)))
+
 (defn formatter
   "Split each line on whitespace, discard nan metric lines
    and format correct lines for each resolution"
   [index rollups input]
-  (let [[path metric time] (s/split (.trim input) #" ")]
-    (when (not= metric "nan")
+  (let [[path metric time] (s/split (.trim input) #" ")
+        metric (parse-num #(Double/parseDouble %) "nan" metric)
+        time (parse-num #(Long/parseLong %) "nan" time)]
+    (when (and (not= metric "nan") (not= time "nan"))
       ;; hardcode empty tenant for now
       (when index (path/register index "" path))
       (for [{:keys [rollup period rollup-to]} rollups]
@@ -21,8 +32,8 @@
          :rollup rollup
          :period period
          :ttl    (* period rollup)
-         :time   (rollup-to (Long/parseLong time))
-         :metric (Double/parseDouble metric)}))))
+         :time   (rollup-to time)
+         :metric metric}))))
 
 (defn handler
   "Send each metric over to the cassandra store"
