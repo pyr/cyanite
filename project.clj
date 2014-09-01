33ae6e3b6fe007317d6ddf0734a814fefd4921f7
Nikita Burtsev <nburtsev@iponweb.net>
2014-09-01 18:42:49 +0400

    version bump (no idea how we are going to sync it across forks)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.spootnik/cyanite "0.1.1"
+(defproject org.spootnik/cyanite "0.2.0"
   :description "Alternative storage backend for graphite, backed by cassandra"
   :url "https://github.com/pyr/cyanite"
   :license {:name "MIT License"
