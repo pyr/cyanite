e867c04a5369514e34a31b2e564ac1df332eca28
Nikita Burtsev <nburtsev@iponweb.net>
2014-09-04 20:11:17 +0400

    update dependencies

--- project.clj
+++ project.clj
@@ -6,23 +6,23 @@
   :maintainer {:email "pyr@spootnik.org"}
   :main org.spootnik.cyanite
   :dependencies [[org.clojure/clojure                       "1.6.0"]
-                 [org.clojure/tools.logging                 "0.2.6"]
-                 [org.clojure/tools.cli                     "0.2.4"]
+                 [org.clojure/tools.logging                 "0.3.0"]
+                 [org.clojure/tools.cli                     "0.3.1"]
                  [org.spootnik/pickler                      "0.1.0"]
-                 [clojurewerkz/elastisch                "2.0.0-rc1"]
-                 [commons-logging/commons-logging           "1.1.3"]
+                 [clojurewerkz/elastisch                    "2.1.0-beta4"]
+                 [commons-logging/commons-logging           "1.2"]
                  [ring/ring-codec                           "1.0.0"]
-                 [aleph                                     "0.3.0"]
+                 [aleph                                     "0.3.3"]
                  [clj-yaml                                  "0.4.0"]
-                 [cc.qbits/alia                             "2.0.0-rc1"]
+                 [cc.qbits/alia                             "2.1.2"]
                  [net.jpountz.lz4/lz4                       "1.2.0"]
-                 [org.xerial.snappy/snappy-java             "1.0.5"]
-                 [org.slf4j/slf4j-log4j12                   "1.6.4"]
-                 [log4j/apache-log4j-extras                 "1.0"]
-                 [io.netty/netty-all "4.0.19.Final"]
+                 [org.xerial.snappy/snappy-java             "1.1.1.3"]
+                 [org.slf4j/slf4j-log4j12                   "1.7.7"]
+                 [log4j/apache-log4j-extras                 "1.2.17"]
+                 [io.netty/netty-all "5.0.0.Alpha1"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [http-kit "2.1.16"]
-                 [log4j/log4j                               "1.2.16"
+                 [http-kit "2.1.19"]
+                 [log4j/log4j                               "1.2.17"
                   :exclusions [javax.mail/mail
                                javax.jms/jms
                                com.sun.jdkmk/jmxtools
