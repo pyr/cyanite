6bf0972f483104c4c3e95fa7170b276e79d346d7
Nikita Burtsev <nburtsev@iponweb.net>
2014-09-10 15:15:43 +0400

    Making tests work again Additional test to check if tenant recognized correctly

--- test/org/spootnik/cyanite/carbon_test.clj
+++ test/org/spootnik/cyanite/carbon_test.clj
@@ -13,7 +13,8 @@
                   :period 3600
                   :ttl    36000
                   :time   500
-                  :metric 2.0})
+                  :metric 2.0
+                  :tenant "NONE"})
            (formatter
                       (assoc-rollup-to  [{:rollup 10 :period 3600}])
                       "foo.bar 2.0 501")))
@@ -23,17 +24,50 @@
                   :period 3600
                   :ttl    36000
                   :time   600
-                  :metric 2.0}
+                  :metric 2.0
+                  :tenant "NONE"}
                  {:path "foo.bar"
                   :rollup 60
                   :period 7200
                   :ttl    432000
                   :time   600
-                  :metric 2.0})
+                  :metric 2.0
+                  :tenant "NONE"})
            (formatter
                       (assoc-rollup-to  [{:rollup 10 :period 3600}
                                          {:rollup 60 :period 7200}])
                       "foo.bar 2.0 601")))
 
 
+    )
+  (testing "rollup-transform-tenants"
+    (is (= (list {:path "foo.bar"
+                  :rollup 10
+                  :period 3600
+                  :ttl    36000
+                  :time   500
+                  :metric 2.0
+                  :tenant "example_tenant"})
+           (formatter
+             (assoc-rollup-to  [{:rollup 10 :period 3600}])
+             "foo.bar 2.0 501 example_tenant")))
+
+    (is (= (list {:path "foo.bar"
+                  :rollup 10
+                  :period 3600
+                  :ttl    36000
+                  :time   600
+                  :metric 2.0
+                  :tenant "example_tenant"}
+                 {:path "foo.bar"
+                  :rollup 60
+                  :period 7200
+                  :ttl    432000
+                  :time   600
+                  :metric 2.0
+                  :tenant "example_tenant"})
+           (formatter
+             (assoc-rollup-to  [{:rollup 10 :period 3600}
+                                {:rollup 60 :period 7200}])
+             "foo.bar 2.0 601 example_tenant")))
     ))
