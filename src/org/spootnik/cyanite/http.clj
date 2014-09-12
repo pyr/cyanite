6769ec34c27adf87e3c3bbdfb8f41266a0ca64fe
Nikita Burtsev <nikita.burtsev@gmail.com>
2014-09-12 19:56:03 +0400

    http cleanup

--- src/org/spootnik/cyanite/http.clj
+++ src/org/spootnik/cyanite/http.clj
@@ -17,24 +17,12 @@
             [clojure.string             :refer [lower-case]]
             [clojure.tools.logging      :refer [info error debug]]))
 
-;(def
-;  ^{:doc "Our dead simple router"}
-;  routes [[:paths   #"^/paths.*"]
-;          [:metrics #"^/metrics.*"]
-;          [:ping #"^/ping/?"]])
 
 (defn now
   "Returns a unix epoch"
   []
   (quot (System/currentTimeMillis) 1000))
 
-;(defn keywordized
-;  "Yield a map where string keys are keywordized"
-;  [params]
-;  (dissoc
-;   (->> (map (juxt (comp keyword lower-case key) val) params)
-;        (reduce merge {}))
-;   nil))
 
 (defn find-best-rollup
   "Find most precise storage period given the oldest point wanted"
@@ -44,81 +32,6 @@
                       rollup-def))]
     (some within (sort-by :rollup rollups))))
 
-;(defn assoc-params
-;  "Parse query args"
-;  [{:keys [query-string] :as request}]
-;  (or
-;   (when-let [params (and (seq query-string)
-;                          (codec/form-decode query-string))]
-;     (assoc request
-;       :params (keywordized
-;                (cond (map? params) params
-;                      (string? params) {params nil}
-;                      :else {}))))
-;   (assoc request :params {})))
-;
-;(defn match-route
-;  ""
-;  [{:keys [uri path-info] :as request} [action re]]
-;  (when (re-matches re (or path-info uri))
-;    action))
-;
-;(defn assoc-route
-;  "Find matching route in router, store "
-;  [request]
-;  (assoc request :action (some (partial match-route request) routes)))
-;
-;(defmulti process
-;  "Process request by dispatching on the action that was found"
-;  :action)
-;
-;(defmethod process :paths
-;  [{{:keys [query tenant]} :params :keys [index] :as request}]
-;  (debug "query now: " query)
-;  (path/prefixes index (or tenant "NONE") (if (str/blank? query) "*" query)))
-;
-;(defmethod process :metrics
-;  [{{:keys [from to path agg tenant]} :params :keys [index store rollups]}]
-;  (debug "fetching paths: " path)
-;  (if-let [{:keys [rollup period]} (find-best-rollup from rollups)]
-;    (let [to    (if to (Long/parseLong to) (now))
-;          from  (Long/parseLong from)
-;          paths (mapcat (partial path/lookup index tenant)
-;                        (if (sequential? path) path [path]))]
-;      (store/fetch store (or agg "mean") paths tenant rollup period from to))
-;    {:step nil :from nil :to nil :series {}}))
-;
-;(defmethod process :ping
-;  [_]
-;  {})
-;
-;(defmethod process :default
-;  [_]
-;  (throw (ex-info "unknown action" {:status 404 :suppress? true})))
-;
-;(defn wrap-process
-;  "Process request, generating a JSON output for it, catch exception
-;   and yield a payload"
-;  [request rollups chan store index]
-;  (debug "got request: " request)
-;  (enqueue
-;   chan
-;   (try
-;     {:status  200
-;      :headers {"Content-Type" "application/json"}
-;      :body    (json/generate-string
-;                (process (assoc request
-;                           :store store
-;                           :rollups rollups
-;                           :index index)))}
-;     (catch Exception e
-;       (let [{:keys [status body suppress?]} (ex-data e)]
-;         (when-not suppress?
-;           (error e "could not process request"))
-;         {:status (or status 500)
-;          :headers {"Content-Type" "application/json"}
-;          :body    (json/generate-string
-;                    (or body {:error (.getMessage e)}))})))))
 
 (defn wrap-local-params [handler params]
   "Adds additional parameters to request"
