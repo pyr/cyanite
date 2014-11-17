e50c3b0aad23e0cf044e630b3047ae54a88f6a34
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-11-17 15:02:28 +0100

    evolve carbon into a transport

--- src/io/cyanite/transport/carbon.clj
+++ src/io/cyanite/transport/carbon.clj
@@ -1,67 +1,47 @@
-(ns io.cyanite.carbon
+(ns io.cyanite.transport.carbon
   "Dead simple carbon protocol handler"
-  (:require [clojure.string        :as s]
-            [io.cyanite.store      :as store]
-            [io.cyanite.path       :as path]
-            [io.cyanite.tcp        :as tcp]
-            [io.cyanite.util       :refer [partition-or-time]]
-            [clojure.tools.logging :refer [info debug]]
-            [clojure.core.async    :as async :refer [<! >! >!! go chan]]))
+  (:require [io.cyanite.transport.tcp :as tcp]
+            [io.cyanite.engine        :as engine]
+            [clojure.core.async       :as a]
+            [clojure.tools.logging    :refer [info debug warn]]
+            [clojure.string           :refer [split]]))
 
 (set! *warn-on-reflection* true)
 
-(defn parse-num
-  "parse a number into the given value, return the
-  default value if it fails"
-  [parse default number]
-  (try (parse number)
-    (catch Exception e
-      (debug "got an invalid number" number (.getMessage e))
-      default)))
-
-(defn formatter
-  "Split each line on whitespace, discard nan metric lines
-   and format correct lines for each resolution"
-  [rollups ^String input]
+(defn time->long
+  [^String t]
   (try
-    (let [[path metric time] (s/split (.trim input) #" ")
-          timel (parse-num #(Long/parseLong %) "nan" time)
-          metricd (parse-num #(Double/parseDouble %) "nan" metric)]
-      (when (and (not= "nan" metricd) (not= "nan" timel))
-          (for [{:keys [rollup period ttl rollup-to]} rollups]
-            {:path   path
-             :rollup rollup
-             :period period
-             :ttl    (or ttl (* rollup period))
-             :time   (rollup-to timel)
-             :metric metricd})))
-      (catch Exception e
-          (info "Exception for metric [" input "] : " e))))
+    (Long/parseLong t)
+    (catch Exception e
+      (warn e "invalid time definition " t))))
 
-(defn format-processor
-  "Send each metric over to the cassandra store"
-  [chan indexch rollups insertch]
-  (go
-    (let [input (partition-or-time 1000 chan 500 5)]
-      (while true
-        (let [metrics (<! input)]
-          (try
-            (doseq [metric metrics]
-              (let [formed (remove nil? (formatter rollups metric))]
-                (doseq [f formed]
-                  (>! insertch f))
-                (doseq [p (distinct (map :path formed))]
-                  (>! indexch p))))
-            (catch Exception e
-              (info "Exception for metric [" metrics "] : " e))))))))
+(defn metric->double
+  [^String m]
+  (try
+    (Double/parseDouble m)
+    (catch Exception e
+      (warn e "invalid double definition " m))))
+
+(defn text->input
+  [^String input]
+  (let [[^String path ^String metric ^String time] (split (.trim input) #" ")
+        out {:path path
+             :metric (metric->double metric)
+             :time   (time->long time)}]
+    out))
 
 (defn start
   "Start a tcp carbon listener"
-  [{:keys [store carbon index]}]
-  (let [indexch (path/channel-for index)
-        insertch (store/channel-for store)
-        chan (chan 100000)
-        handler (format-processor chan indexch (:rollups carbon) insertch)]
-    (info "starting carbon handler: " carbon)
-    (tcp/start-tcp-server
-     (merge carbon {:response-channel chan}))))
+  [config core]
+  (let [chan   (a/chan 100000)
+        mapped (a/remove< nil? (a/map< text->input chan))]
+    (reify
+      engine/Service
+      (start! [this]
+        (info "starting carbon handler")
+        (tcp/start-tcp-server
+         (merge config {:response-channel chan})))
+      clojure.lang.ILookup
+      (valAt [this k]
+        (when (= k :channel)
+          mapped)))))
