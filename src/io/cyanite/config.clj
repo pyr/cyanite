44fb5b2797beea828a19f0e07e523af94a7ceb36
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-11-17 14:58:59 +0100

    new config logic, building transports

--- src/io/cyanite/config.clj
+++ src/io/cyanite/config.clj
@@ -3,6 +3,7 @@
   (:require [clj-yaml.core          :refer [parse-string]]
             [clojure.string         :refer [split]]
             [org.spootnik.logconfig :refer [start-logging!]]
+            [io.cyanite.precision   :refer [map->Precision]]
             [clojure.tools.logging  :refer [error info debug]]))
 
 (def
@@ -17,11 +18,12 @@
 
 (def ^{:doc "handle storage with cassandra-metric-store by default"}
   default-store
-  {:use "io.cyanite.store/cassandra-metric-store"})
+  {:use "io.cyanite.store.cassandra/cassandra-store"})
 
 (def ^{:doc "let carbon listen on 2003 by default"}
-  default-carbon
+  default-transport
   {:enabled true
+   :use     "io.cyanite.transport.carbon/start"
    :host    "127.0.0.1"
    :port    2003
    :readtimeout 30})
@@ -33,7 +35,7 @@
    :port    8080})
 
 (def default-index
-  {:use "io.cyanite.path/memory-pathstore"})
+  {:use "io.cyanite.index.atom/atom-index"})
 
 (defn to-seconds
   "Takes a string containing a duration like 13s, 4h etc. and
@@ -68,14 +70,6 @@
   [rollups]
   (map convert-shorthand-rollup rollups))
 
-(defn assoc-rollup-to
-  "Enhance a rollup definition with a function to compute
-   the rollup of a point"
-  [rollups]
-  (map (fn [{:keys [rollup] :as rollup-def}]
-         (assoc rollup-def :rollup-to #(-> % (quot rollup) (* rollup))))
-       rollups))
-
 (defn find-ns-var
   "Find a symbol in a namespace"
   [s]
@@ -87,19 +81,28 @@
       (prn "Exception: " e))))
 
 (defn instantiate
-  "Find a symbol pointing to a function of a single argument and
-   call it"
-  [class config]
-  (if-let [f (find-ns-var class)]
-    (f config)
-    (throw (ex-info (str "no such namespace: " class) {}))))
+  "Find a symbol pointing to a function of a single argument.
+   If arguments are provided, call the function"
+  ([class]
+     (or (find-ns-var class)
+         (throw (ex-info (str "no such namespace: " class) {}))))
+  ([class config args]
+     (apply (instantiate class) config args)))
 
 (defn get-instance
   "For dependency injected configuration elements, find build fn
    and call it"
-  [{:keys [use] :as config} target]
+  [{:keys [use] :as config} target & args]
   (debug "building " target " with " use)
-  (instantiate (-> use name symbol) config))
+  (instantiate (-> use name symbol) config args))
+
+(defn get-sym
+  "For dependency injected configuration elements, find build fn
+   and call it"
+  [{:keys [use type] :as config}]
+  (debug "fetching" use)
+  (assoc config :constructor (instantiate (-> use name symbol))))
+
 (defn load-path
   "Try to find a pathname, on the command line, in
    system properties or the environment and load it."
@@ -117,16 +120,20 @@
   (try
     (when-not quiet?
       (println "starting with configuration: " path))
-    (let [config (load-path path)]
+    (let [config           (load-path path)
+          update-transport (comp
+                            get-sym
+                            (partial merge default-transport))]
+
       (start-logging! (merge default-logging (:logging config)))
-      (-> (load-path path)
+      (-> config
 
           (update-in [:store] (partial merge default-store))
           (update-in [:store] get-instance :store)
+          (update-in [:precisions] (partial mapv map->Precision))
+          (update-in [:transports] (partial mapv update-transport))
 
-          (update-in [:carbon] (partial merge default-carbon))
-          (update-in [:carbon :rollups] convert-shorthand-rollups)
-          (update-in [:carbon :rollups] assoc-rollup-to)
           (update-in [:index] (partial merge default-index))
           (update-in [:index] get-instance :index)
+
           (update-in [:http] (partial merge default-http))))))
