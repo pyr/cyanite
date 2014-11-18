b3e91cb36ba3e7dc1b75be0f75ddd0b5d8adc856
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-11-18 11:05:16 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -10,14 +10,14 @@
                  [org.clojure/tools.logging     "0.3.1"]
                  [org.clojure/tools.cli         "0.3.1"]
                  [org.spootnik/pickler          "0.1.4"]
-                 [org.spootnik/logconfig        "0.7.2"]
+                 [org.spootnik/logconfig        "0.7.3"]
                  [ring/ring-codec               "1.0.0"]
                  [clj-yaml                      "0.4.0"]
                  [clj-http                      "1.0.1"
                   :exclusions [commons-codec]]
                  [cc.qbits/jet                  "0.5.0-alpha3"]
-                 [cc.qbits/alia                 "2.2.2"]
+                 [cc.qbits/alia                 "2.2.3"]
                  [net.jpountz.lz4/lz4           "1.2.0"]
-                 [org.xerial.snappy/snappy-java "1.1.1.3"]
+                 [org.xerial.snappy/snappy-java "1.1.1.6"]
                  [io.netty/netty-all            "4.0.19.Final"]
                  [org.clojure/core.async        "0.1.346.0-17112a-alpha"]])
