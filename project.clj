4f13d5b79bfe544569f928518f4e7edd47050b89
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-06-16 14:05:49 +0200

    store: wrong optional args

--- project.clj
+++ project.clj
@@ -6,19 +6,20 @@
   :maintainer {:email "pyr@spootnik.org"}
   :aot :all
   :main io.cyanite
-  :dependencies [[org.clojure/clojure           "1.7.0-alpha2"]
+  :plugins [[lein-ancient "0.6.7"]]
+  :dependencies [[org.clojure/clojure           "1.7.0-beta3"]
                  [org.clojure/tools.logging     "0.3.1"]
                  [org.clojure/tools.cli         "0.3.1"]
                  [org.spootnik/pickler          "0.1.0"]
-                 [org.spootnik/logconfig        "0.7.2"]
+                 [org.spootnik/logconfig        "0.7.3"]
                  [ring/ring-codec               "1.0.0"]
                  [clj-yaml                      "0.4.0"]
                  [clj-http                      "1.0.1"
                   :exclusions [commons-codec]]
-                 [cc.qbits/jet                  "0.5.5"]
-                 [cc.qbits/alia                 "2.2.2"]
-                 [net.jpountz.lz4/lz4           "1.2.0"]
-                 [org.xerial.snappy/snappy-java "1.1.1.3"]
+                 [cc.qbits/jet                  "0.6.2"]
+                 [cc.qbits/alia                 "2.3.9"]
+                 [net.jpountz.lz4/lz4           "1.3"]
+                 [org.xerial.snappy/snappy-java "1.1.1.7"]
                  [io.netty/netty-all            "4.0.19.Final"]
                  [org.clojure/core.async        "0.1.346.0-17112a-alpha"]
                  [clojurewerkz/elastisch        "2.0.0"
