f3ac709f734723c3bbe57941968ace9ba884da81
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-07-23 11:01:35 +0200

    api: use wrapping function

--- src/io/cyanite/api.clj
+++ src/io/cyanite/api.clj
@@ -91,7 +91,7 @@
                            (if (sequential? path) path [path]))
                    (map (partial engine/resolution engine from))
                    (remove nil?))]
-    (store/fetch! store from to paths)))
+    (store/query! store from to paths)))
 
 (defmethod dispatch :default
   [_]
