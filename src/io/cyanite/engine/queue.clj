95f9273269f9f49f3e18b0e682896ce0dcc9eb03
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-10-02 18:07:16 +0200

    queue: use a FixedThreadPool directly

--- src/io/cyanite/engine/queue.clj
+++ src/io/cyanite/engine/queue.clj
@@ -1,8 +1,9 @@
 (ns io.cyanite.engine.queue
   "Queueing mechanism at the heart of cyanite.
    This is how we "
-  (:require [com.climate.claypoole :as cp]
-            [clojure.tools.logging :refer [warn]]))
+  (:import java.util.concurrent.Executors
+           java.util.ArrayList)
+  (:require [clojure.tools.logging :refer [warn]]))
 
 (defonce default-poolsize 4)
 
@@ -11,10 +12,28 @@
   (add! [this k e])
   (stop! [this k]))
 
+(defn threadpool
+  [sz]
+  (Executors/newFixedThreadPool (int sz)))
+
+(defn pool-future-call
+  [p f]
+  (.submit p f))
+
+(defmacro with-future
+  [p & body]
+  `(pool-future-call ~p (fn [] ~@body)))
+
 (defn linked-queue
   [opts]
   (java.util.concurrent.ArrayBlockingQueue.
    (int (or (:queue-capacity opts) 10e3))))
+
+(defn drain!
+  [q]
+  (let [al (ArrayList.)]
+    (.drainTo q al)
+    al))
 
 (defn queue-engine
   [opts]
@@ -22,19 +41,19 @@
     (reify QueueEngine
       (consume! [this k opts f]
         (let [sz   (or (:pool-size opts) default-poolsize)
-              pool (cp/threadpool sz)
+              pool (threadpool sz)
               q    (linked-queue opts)]
           (swap! state assoc k {:pool pool :queue q})
           (dotimes [i sz]
-            (cp/future
-              pool
-              (loop [elem (.take q)]
+            (with-future pool
+              (loop [elems (drain! q)]
                 (try
-                  (f elem)
+                  (doseq [elem elems]
+                    (f elem))
                   (catch Exception e
                     (warn e "could not process element on queue" i "for" k)
                     (warn (:exception (ex-data e)) "original exception")))
-                (recur (.take q)))))))
+                (recur (drain! q)))))))
       (add! [this k e]
         (if-let [q (get-in @state [k :queue])]
           (.add q e)
