45761100f62b9a6630819bc3681937c95afd8916
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-02-05 10:31:29 +0100

    queue: store a fixed number of queues.
    With a faster queue like disruptor's, we can avoid costly derefs
    and instead store our two fixed queues: ingestq and writeq.
    
    The QueueEngine protocol is now reified by the queues themselves,
    the component only acts as a holder for both ingestq and writeq.

--- src/io/cyanite/engine/queue.clj
+++ src/io/cyanite/engine/queue.clj
@@ -14,12 +14,14 @@
 
 (defcounter cnt-queue)
 (defcounter cnt-errors)
+
 (defonce default-poolsize 4)
 (defonce default-capacity (int 1024))
 
 (defprotocol QueueEngine
-  (consume! [this k opts f])
-  (add! [this k e]))
+  (shutdown! [this])
+  (consume! [this f])
+  (add! [this e]))
 
 (defn make-event-factory
   [ctor]
@@ -50,17 +52,9 @@
   [sz]
   (Executors/newFixedThreadPool (int sz)))
 
-(defn pool-future-call
-  [p f]
-  (.submit p f))
-
-(defmacro with-future
-  [p & body]
-  `(pool-future-call ~p (fn [] ~@body)))
-
 (defn disruptor
   [event-factory size executor]
-  (Disruptor. event-factory size executor))
+  (Disruptor. event-factory (int size) executor))
 
 (defn make-translator
   [f]
@@ -68,35 +62,34 @@
     (translateTo [this event sequence arg0]
       (f event arg0))))
 
-(defrecord BlockingMemoryQueue [state defaults]
+(defn event-handler
+  [f]
+  (reify EventHandler
+    (onEvent [this event sequence eob]
+      (f (get-data event)))))
+
+(defrecord DisruptorQueue [disruptor translator]
+  QueueEngine
+  (shutdown! [this]
+    (.shutdown disruptor))
+  (add! [this e]
+    (.publishEvent disruptor translator e))
+  (consume! [this f]
+    (.handleEventsWith disruptor (into-array EventHandler [(event-handler f)]))
+    (.start disruptor)))
+
+(defn make-queue
+  [defaults]
+  (let [capacity (or (:queue-capacity defaults) default-capacity)
+        pool     (threadpool (or (:pool-size defaults) default-poolsize))]
+    (DisruptorQueue. (disruptor (wrapper-event-factory) capacity pool)
+                     (make-translator (fn [e v] (set-data e v))))))
+
+(defrecord BlockingMemoryQueue [ingestq writeq defaults]
   component/Lifecycle
   (start [this]
-    (assoc this
-           :state (atom {})
-           ))
+    (assoc this :ingestq (make-queue defaults) :writeq  (make-queue defaults)))
   (stop [this]
-    (doseq [[_ {:keys [[pool disruptor]]}] @state]
-      (.shutdown disruptor)
-      (.shutdown pool))
-    (assoc this :state nil))
-  QueueEngine
-  (consume! [this k opts f]
-    (let [sz   (or (:pool-size opts)
-                   (get-in defaults [k :pool-size])
-                   default-poolsize)
-          pool (threadpool sz)
-          cap       (or (:queue-capacity opts)
-                        (get-in defaults [k :queue-capacity])
-                        default-capacity)
-          disruptor (disruptor (wrapper-event-factory) cap pool)]
-      (swap! state assoc k {:disruptor  disruptor
-                            :translator (make-translator (fn [e v]  (set-data e v)))})
-      (.handleEventsWith disruptor
-                         (into-array [(reify EventHandler
-                                         (onEvent [this event sequence eob]
-                                           (f (get-data event))))]))
-      (.start disruptor)))
-  (add! [this k e]
-    ;; TODO: dereferencing is an expensive operation going through the atomic memory barrier
-    (let [{:keys [disruptor translator]} (get @state k)]
-      (.publishEvent disruptor translator e))))
+    (shutdown! ingestq)
+    (shutdown! writeq)
+    (assoc this :ingestq nil :writeq nil)))
