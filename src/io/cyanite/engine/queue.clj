9c58756b786fe8a5b57664df41d149260c51bb25
Alex Petrov <oleksandr.petrov@gmail.com>
2016-02-09 19:22:27 +0100

    Add instrumentation to queues

--- src/io/cyanite/engine/queue.clj
+++ src/io/cyanite/engine/queue.clj
@@ -6,13 +6,16 @@
            com.lmax.disruptor.dsl.Disruptor
            com.lmax.disruptor.EventFactory
            com.lmax.disruptor.EventTranslatorOneArg
+           com.lmax.disruptor.ExceptionHandler
            com.lmax.disruptor.EventHandler)
   (:require [com.stuartsierra.component :as component]
-            [clojure.tools.logging      :refer [warn]]
+            [clojure.tools.logging      :refer [warn error]]
             [metrics.counters           :refer [defcounter inc! dec!]]))
 
-(defcounter cnt-queue)
-(defcounter cnt-errors)
+(defcounter cnt-ingestq)
+(defcounter cnt-writeq)
+(defcounter cnt-errors-ingestq)
+(defcounter cnt-errors-writeq)
 
 (defonce default-poolsize 4)
 (defonce default-capacity (int 1024))
@@ -48,28 +51,49 @@
     (onEvent [this event sequence eob]
       (f @event))))
 
-(defrecord DisruptorQueue [disruptor translator]
+(defn exception-handler
+  [f]
+  (reify ExceptionHandler
+    (handleEventException [this ex sequence event]
+      (f [ex event]))
+    (handleOnShutdownException [this ex]
+      ;; no-op
+      )
+    (handleOnStartException [this ex]
+      ;; no-op
+      )))
+
+(defrecord DisruptorQueue [disruptor translator cnt error-cnt]
   QueueEngine
   (shutdown! [this]
     (.shutdown disruptor))
   (add! [this e]
     (.publishEvent disruptor translator e))
   (consume! [this f]
-    (.handleEventsWith disruptor (into-array EventHandler [(event-handler f)]))
+    (.handleEventsWith disruptor (into-array EventHandler [(event-handler (fn [e]
+                                                                            (inc! cnt)
+                                                                            (f e)))]))
+    (.handleExceptionsWith disruptor (exception-handler (fn [[ex event]]
+                                                          (inc! error-cnt)
+                                                          (error ex "could not handle an event"))))
     (.start disruptor)))
 
 (defn make-queue
-  [defaults]
+  [defaults cnt error-cnt]
   (let [capacity (or (:queue-capacity defaults) default-capacity)
         pool     (threadpool (or (:pool-size defaults) default-poolsize))
         factory  (make-event-factory #(volatile! nil))]
     (DisruptorQueue. (disruptor factory capacity pool)
-                     (make-translator vreset!))))
+                     (make-translator vreset!)
+                     cnt
+                     error-cnt)))
 
 (defrecord BlockingMemoryQueue [ingestq writeq defaults]
   component/Lifecycle
   (start [this]
-    (assoc this :ingestq (make-queue defaults) :writeq  (make-queue defaults)))
+    (assoc this
+           :ingestq (make-queue defaults cnt-ingestq cnt-errors-ingestq)
+           :writeq  (make-queue defaults cnt-writeq cnt-errors-writeq)))
   (stop [this]
     (shutdown! ingestq)
     (shutdown! writeq)
