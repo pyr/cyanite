b699bb1de3c3d03cc239dbdf9ad0d4d2c9fbc578
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-02-09 22:40:15 +0100

    cyanite: bump alia dependency

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                  [metrics-clojure               "2.6.1"]
                  [clj-yaml                      "0.4.0"]
                  [clj-time                      "0.11.0"]
-                 [cc.qbits/alia                 "2.10.0"]
+                 [cc.qbits/alia                 "2.12.1"]
                  [com.lmax/disruptor            "3.3.4"]
                  [com.boundary/high-scale-lib   "1.0.6"]
                  [net.jpountz.lz4/lz4           "1.3"]
