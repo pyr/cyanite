11b59d57660c47f77f229bed1a496d9f609ce705
Alex Petrov <oleksandr.petrov@gmail.com>
2016-02-11 23:26:43 +0100

    Refactor test system, use full dependency map. Bring the engine test back.

--- test/io/cyanite/engine_test.clj
+++ test/io/cyanite/engine_test.clj
@@ -5,24 +5,23 @@
             [io.cyanite.test-helper     :refer :all]))
 
 
-(comment
-  (deftest engine-test
-    (with-config
-      {:engine (component/using (e/map->Engine {:rules {"default" ["5s:1h"]}}) [:drift
-                                                                                :queues
-                                                                                :writer])}
-      (let [engine    (:engine *system*)
-            clock     (:clock *system*)
-            writer    (:writer *system*)
-            base-time 1454877020]
-        (doseq [i (range 0 6)]
-          (set-time! clock (* (+ base-time i) 1000))
-          (e/accept! engine {:path "a.b.c" :metric i :time (+ base-time i)}))
+(deftest engine-test
+  (with-config
+    {:engine {:rules {"default" ["5s:1h"]}}}
+    {}
+    (let [engine    (:engine *system*)
+          clock     (:clock *system*)
+          writer    (:writer *system*)
 
-        (let [[res] @writer]
-          (is (= "a.b.c" (:path res)))
-          (is (= 2.0 (:mean res)))
-          (is (= 10 (:sum res)))
-          (is (= 4 (:max res)))
-          (is (= 0 (:min res)))
-          (is (= base-time (:time res))))))))
+          base-time 1454877020]
+      (doseq [i (range 0 6)]
+        (set-time! clock (* (+ base-time i) 1000))
+        (e/ingest! engine {:path "a.b.c" :metric i :time (+ base-time i)}))
+
+      (let [[res] (e/snapshot! engine)]
+        (is (= "a.b.c" (:path res)))
+        (is (= 2.0 (:mean res)))
+        (is (= 10 (:sum res)))
+        (is (= 4 (:max res)))
+        (is (= 0 (:min res)))
+        (is (= base-time (:time res)))))))
