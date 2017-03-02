3847acc70d7a1b351d393fad84e7f52d397d89e1
Pierre-Yves Ritschard <pyr@spootnik.org>
2017-03-02 09:54:45 +0100

    bump dependencies

--- project.clj
+++ project.clj
@@ -5,28 +5,28 @@
             :url "https://github.com/pyr/cyanite/tree/master/LICENSE"}
   :maintainer {:email "pyr@spootnik.org"}
   :profiles {:uberjar {:aot      :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:global-vars {*warn-on-reflection* true}}}
   :main io.cyanite
   :plugins [[lein-ancient "0.6.7"]]
-  :dependencies [[org.clojure/clojure                    "1.8.0"]
+  :dependencies [[org.clojure/clojure                    "1.9.0-alpha14"]
                  [org.clojure/tools.logging              "0.3.1"]
                  [org.clojure/tools.cli                  "0.3.3"]
-                 [com.stuartsierra/component             "0.3.1"]
-                 [spootnik/unilog                        "0.7.13"]
+                 [com.stuartsierra/component             "0.3.2"]
+                 [spootnik/unilog                        "0.7.17"]
                  [spootnik/uncaught                      "0.5.3"]
                  [spootnik/globber                       "0.4.1"]
-                 [spootnik/reporter                      "0.1.5"]
-                 [spootnik/signal                        "0.2.0"]
-                 [org.javassist/javassist                "3.20.0-GA"]
-                 [instaparse                             "1.4.1"]
-                 [cheshire                               "5.5.0"]
-                 [metrics-clojure                        "2.6.1"]
+                 [spootnik/reporter                      "0.1.17"]
+                 [spootnik/signal                        "0.2.1"]
+                 [org.javassist/javassist                "3.21.0-GA"]
+                 [instaparse                             "1.4.5"]
+                 [cheshire                               "5.7.0"]
                  [clj-yaml                               "0.4.0"]
-                 [clj-time                               "0.11.0"]
-                 [com.github.ben-manes.caffeine/caffeine "2.3.4"]
-                 [cc.qbits/alia                          "3.2.0"]
-                 [org.jctools/jctools-core               "1.2"]
+                 [clj-time                               "0.13.0"]
+                 [com.github.ben-manes.caffeine/caffeine "2.4.0"]
+                 [cc.qbits/alia                          "4.0.0-beta7"]
+                 [org.jctools/jctools-core               "2.0.1"]
                  [com.boundary/high-scale-lib            "1.0.6"]
-                 [net.jpountz.lz4/lz4                    "1.3"]
-                 [org.xerial.snappy/snappy-java          "1.1.2.1"]
-                 [io.netty/netty-all                     "4.0.34.Final"]])
+                 [net.jpountz.lz4/lz4                    "1.3.0"]
+                 [org.xerial.snappy/snappy-java          "1.1.2.6"]
+                 [io.netty/netty-all                     "4.1.8.Final"]])
